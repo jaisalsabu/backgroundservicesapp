60d4b776dbfa76e28fe198fbe621db2e4c3bccda
jaisalsabu <jaisal1010sabu@gmail.com>
2020-03-14 14:00:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/backgroundservicesapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.backgroundservicesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonStart;
+    private Button buttonStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        //attaching onclicklistener to buttons
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonStart) {
+            startService(new Intent(this, Myservice.class));
+        } else if (view == buttonStop) {
+            stopService(new Intent(this, Myservice.class));
+        }
+    }
+}
